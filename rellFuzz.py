d75998089dea0bded0e6e2c1921586404ccb0d5f
gregrell <rell.greg@gmail.com>
2020-03-01 20:47:33 -0500

    Restructured code, ability to call from shell base code implemented

--- rellFuzz.py
+++ rellFuzz.py
@@ -3,12 +3,7 @@
 #Read In JPG File
 from array import array
 import os
-jpgData = array('B')
-
-filesize = os.path.getsize('cross.jpg')
-
-
-
+import subprocess
 
 
 #modifying routine to modify the character array
@@ -19,13 +14,29 @@ def modify(orignalData):
         newData[i] = orignalData[i]
     return newData
 
+def openFile(name):
+    with open(name, 'rb') as f:
+        jpgData = array('B')
+        filesize = os.path.getsize(name)
+        jpgData.fromfile(f,filesize)
+    return jpgData
 
-#open the cross.jpg file
-with open('cross.jpg', 'rb') as f:
-    jpgData.fromfile(f,filesize)
 
-#write to a temp.jpg file with a character array that has been
-#modified from the original cross.jpg file
-    with open('temp.jpg','wb') as outFile:
-        outFile.write(modify(jpgData))
+def writeFile(name,data):
+    with open(name, 'wb') as outFile:
+        outFile.write(data)
 
+
+def main():
+    #open the cross.jpg file
+    crossJpg=openFile('cross.jpg')
+
+    #write to a temp.jpg file with a character array that has been
+    #modified from the original cross.jpg file
+    writeFile('temp.jpg',modify(crossJpg))
+    ret_code = subprocess.call("./jpg2bmp", shell=True)
+    print(ret_code)
+
+
+if __name__ == "__main__":
+    main()
